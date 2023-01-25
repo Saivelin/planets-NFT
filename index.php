d8a2129e76e2fe4114b98ee727bd436946eeb760
Saivelin <vlad.pochkalov@mail.ru>
2023-01-25 23:19:31 +0300

    first steps

--- index.php
+++ index.php
@@ -33,10 +33,40 @@
             <p> LINKS</p>
         </div>
         <div class="headerItem headerItemL">
-            <img src="/img/logo.svg" alt="">
-            <img src="/img/logo.svg" alt="">
+            <img src="/img/globe.svg" alt="">
+            <img src="/img/sunny.svg" alt="">
         </div>
     </header>
+    <main class="main">
+        <div class="plBl"></div>
+        <div class="SmartTokenBL">
+            <div class="headersSTBl">
+                <h1 class="headerSTBl">SMARTTOKEN</h1>
+                <h3 class="headerSTBlMini">TOKENOMICS <br>
+                    20 000 000 $SMART</h3>
+            </div>
+            <div class="mainSmartTokenBL">
+                <div>
+                    <p class="mainSmartTokenBLTxt">Универсальный токен для использования во всех сервисах команды Smart
+                        Team
+                        на блокчейне TON.
+                        <br><br>
+                        Это внутренняя валюта во всех существующих проектах входящих в экосистему Smart Team. Например,
+                        токеном можно будет приобрести NFT Smart Tigers, оплатить комиссию в Resale Market, но основной
+                        задачей данного токена будет является оплата продвижения и дополнительных услуг в Smart Working.
+                        <br><br>
+                        Со временем токен будет внедряться во все наши
+                        и партнерские проекты как способ оплаты.
+                    </p>
+                    <div class="smttBack">
+                        <h6 class="smttBackH">SMART on TON Chain</h6>
+                        <p class="smttBackt">EQB8OK7t8M...70GEULQ6zM</p>
+                    </div>
+                </div>
+                <img src="/img/st.svg" alt="">
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>

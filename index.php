9000ee807f33c484eb1047c1d4a5658b7b71eb7a
Saivelin <vlad.pochkalov@mail.ru>
2023-01-25 22:14:53 +0300

    first steps

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Token</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="headerItem headerItemF">
+            <img src="/img/logo.svg" alt="">
+        </div>
+        <div class="headerItem">
+            <p> SMARTTOKEN</p>
+        </div>
+        <div class="headerItem">
+            <p> NFT</p>
+        </div>
+        <div class="headerItem">
+            <p> ROAD MAP</p>
+        </div>
+        <div class="headerItem">
+            <p> TEAM</p>
+        </div>
+        <div class="headerItem">
+            <p> WHITE PAPER</p>
+        </div>
+        <div class="headerItem">
+            <p> LINKS</p>
+        </div>
+        <div class="headerItem headerItemL">
+            <img src="/img/logo.svg" alt="">
+            <img src="/img/logo.svg" alt="">
+        </div>
+    </header>
+</body>
+
+</html>

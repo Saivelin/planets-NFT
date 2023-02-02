701ad73f590947d008db1c8fe03928c61db44a29
Saivelin <vlad.pochkalov@mail.ru>
2023-02-02 18:16:56 +0300

    bug fix

--- test/main.php
+++ test/main.php
@@ -1,2 +1,17 @@
 <?php
-array_map('unlink', glob(dirname(__FILE__) . '/test/*'));
+$dir = opendir(dirname(__FILE__));
+$count = 0;
+while ($file = readdir($dir)) {
+    if ($file == '.' || $file == '..' || is_dir('path/to/dir' . $file)) {
+        continue;
+    }
+    $count++;
+}
+echo ($count);
+echo dirname(__FILE__);
+// $i = 0;
+// while ($i <= $count) {
+//     unlink(__DIR__ . __FILE__);
+// }
+// unlink(dirname(__FILE__) . "\img");
+array_map('unlink', glob(dirname(__FILE__) . '/test/.'));
